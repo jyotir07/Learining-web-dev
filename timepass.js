01907829183c1435c28affd50f4f15d801fc3261
Jyotiraditya Singh <77264025+jyotir07@users.noreply.github.com>
2023-10-16 22:50:49 +0530

    Create timepass.js

---
+++ timepass.js
@@ -0,0 +1,13 @@
+function something(vibes, nice){
+  if(vibes >= nice){
+    return "Vibe check";
+  }
+  else{
+    return "Vibes nahi aa rahi";
+  }
+}
+const vibes = 12;
+const nice = 5;
+something(vibes, nice);
+
+console.log("Peace out ✌️")
